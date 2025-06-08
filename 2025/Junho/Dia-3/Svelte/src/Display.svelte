8e5587829b8c896caa9e1841a8c10375b74ae04e
Alexandre Schimainda Stempinhaki <alex04schi@gmail.com>
2025-06-08 19:42:19 -0300

    Commit muito loko, muito loko mesmo piazada

---
+++ 2025/Junho/Dia-3/Svelte/src/Display.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let display = '0';
+</script>
+
+<input readonly bind:value={display}/>
+
+<style>
+	input {
+		width: 20vw;
+		height: 7vh;
+		border: 4px solid darkblue;
+		font-size: 2vw;
+		font-weight: bold;
+	}
+</style>
---
+++ 2025/Junho/Dia-3/Svelte/src/Keyboard.svelte
@@ -0,0 +1,46 @@
+<script>
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+	const buttons = [
+		'1', '2', '3', '+',
+		'4', '5', '6', '-',
+		'7', '8', '9', '*',
+		'=', '0', 'C', '/'
+	]
+
+    function enviar(btn){
+        dispatch('botaoClicado', btn);
+    }
+
+</script>
+
+		<div class = "botoes">
+			{#each buttons as btn}
+				<button on:click={() => enviar(btn)}>{btn}</button>
+			{/each}
+		</div>
+
+<style>
+	.botoes{
+		width: 20.8vw;
+		display: flex;
+		flex-wrap: wrap;
+		}
+
+	button{
+		width: 4.7vw;
+		margin: 0.25vw;
+		background-color: darkblue;
+		font-size: 2vw;
+		color: white;
+		font-weight: bold;
+		border-radius: 1vw;
+	}
+
+	button:hover{
+		opacity: 0.5;
+	}
+</style>
